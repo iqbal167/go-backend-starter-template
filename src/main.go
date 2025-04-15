a4bb7bfa90dda3341e84399ad8963ece72051094
Muhammad Iqbal Ali <muhammadiqbalali167@gmail.com>
2025-04-15 17:53:40 +0700

    refactor: replace health handler with config-based response

--- src/main.go
+++ src/main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"fmt"
+	"go-backend-starter-kit/src/config"
 	"log"
 	"net/http"
 )
 
-func healthHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "OK")
-}
-
 func main() {
-	http.HandleFunc("/healthz", healthHandler)
+	config := config.Load()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "app_name=%s version=%s", config.App.Name, config.App.Version)
+	})
 
 	addr := fmt.Sprintf("0.0.0.0:%d", 8080)
 
@@ -20,7 +20,7 @@ func main() {
 		Handler: nil,
 	}
 
-	log.Printf("Server started at http://%s", server.Addr)
+	log.Printf("Server started at http://%s", addr)
 	if err := server.ListenAndServe(); err != nil {
 		log.Fatalf("could not start server: %v", err)
 	}
